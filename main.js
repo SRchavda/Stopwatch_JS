7d8a1a71a1fca3b4283cb7d455d6f2d624036224
SRchavda <shivrajchavda1008@gmail.com>
2024-10-05 13:14:04 +0530

    add styling

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ var resetButton = document.getElementById("reset-btn");
 var hourNode = document.getElementById("time-hour");
 var minNode = document.getElementById("time-min");
 var secNode = document.getElementById("time-sec");
+var miliSecNode = document.getElementById("time-milisec");
 
 let intervalId;
 let isRunning = false;
+let elapsedMiliSeconds = 0;
 let elapsedSeconds = 0;
 let elapsedMinutes = 0;
 let elapsedHours = 0;
@@ -17,44 +19,52 @@ let elapsedHours = 0;
 const startWatch = () => {
   clearInterval(intervalId);
   if (isRunning) {
-    //pause the timer
+    //pause the watch if running
     startButton.innerText = "Resume";
   } else {
-    //start the timer
+    //start the new watch if not already running
     intervalId = setInterval(function () {
-      elapsedSeconds++;
-      updateHtml(elapsedSeconds);
-    }, 1000);
+      elapsedMiliSeconds = elapsedMiliSeconds + 1;
+      updateHtml(elapsedMiliSeconds);
+    }, 10);
     startButton.innerText = "Pause";
+    stopButton.classList.remove("d-none");
+    resetButton.classList.remove("d-none");
   }
-  isRunning = !isRunning;
+  isRunning = !isRunning; //invert the status each time
 };
 
 const stopWatch = () => {
-  if (!intervalId) return;
-  console.log(
-    `Last Counted Time was - ${elapsedHours} : ${elapsedMinutes} : ${elapsedSeconds}`
-  );
-  clearInterval(intervalId);
-  elapsedSeconds = 0;
-  elapsedMinutes = 0;
-  elapsedHours = 0;
-  startButton.innerText = "Start";
-  isRunning = false;
+  if (!intervalId) return; //if already stopped then return
+  resetTimer(); //else just reset time
 };
 
 const resetWatch = () => {
+  resetTimer(); //reset time
+  updateHtml(elapsedSeconds); //update web page with reseted time
+
+  //hide stop & reset button
+  stopButton.classList.add("d-none");
+  resetButton.classList.add("d-none");
+};
+
+const resetTimer = () => {
   clearInterval(intervalId);
+  elapsedMiliSeconds = 0;
   elapsedSeconds = 0;
   elapsedMinutes = 0;
   elapsedHours = 0;
-  updateHtml(elapsedSeconds);
   startButton.innerText = "Start";
   isRunning = false;
 };
 
-const updateHtml = (currentSeconds) => {
-  if (currentSeconds == 60) {
+const updateHtml = (currentMiliSec) => {
+  //calculate timer
+  if (currentMiliSec == 100) {
+    elapsedSeconds++;
+    elapsedMiliSeconds = 0;
+  }
+  if (elapsedSeconds == 60) {
     elapsedMinutes++;
     elapsedSeconds = 0;
   }
@@ -63,11 +73,14 @@ const updateHtml = (currentSeconds) => {
     elapsedMinutes = 0;
   }
 
+  //add padding to time and update web page
+  miliSecNode.innerText = addPad(elapsedMiliSeconds);
   secNode.innerHTML = addPad(elapsedSeconds);
   minNode.innerHTML = addPad(elapsedMinutes);
   hourNode.innerHTML = addPad(elapsedHours);
 };
 
+//to append padding to number
 const addPad = (count) => {
   return count <= 9 ? `0${count}` : count;
 };
